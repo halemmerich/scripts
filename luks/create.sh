27f9a437434eee0420cf37f70e77eb2a75353f9d
Martin Boonk <martin@boonk.info>
2016-08-16 10:37:35 +0200

    More output and possibility of abort

--- luks/create.sh
+++ luks/create.sh
@@ -44,6 +44,14 @@ then
 	exit 1
 fi
 
-dd if=/dev/urandom bs=8192 count=1 of=$KEYFILE
-dd if=/dev/zero bs=1049600 count=1 of=$HEADERFILE
-cryptsetup luksFormat --header $HEADERFILE --key-file $KEYFILE --align-payload 0 $BLOCKDEVICE
+echo Creating keyfile $KEYFILE, headerfile $HEADERFILE and encrypting blockdevice $BLOCKDEVICE.
+
+read -p "Continue? [y/n] " -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+	dd if=/dev/urandom bs=8192 count=1 of="$KEYFILE"
+	dd if=/dev/zero bs=1049600 count=1 of="$HEADERFILE"
+	cryptsetup luksFormat --header "$HEADERFILE" --key-file "$KEYFILE" --align-payload 0 $BLOCKDEVICE
+fi
+
